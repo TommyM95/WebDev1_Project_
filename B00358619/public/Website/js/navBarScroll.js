208cfef80b619d38eedcc2ad403d69395a66afff
Tommy <mcgillt95@gmail.com>
2021-10-27 16:13:33 +0100

    1

--- B00358619/public/Website/js/navBarScroll.js
+++ B00358619/public/Website/js/navBarScroll.js
@@ -2,7 +2,7 @@ myID = document.getElementById("myID");
 
 var myScrollFunc = function () {
     var y = window.scrollY;
-    if (y >= 100) {
+    if (y >= 350) {
         myID.className = "bottomMenu show"
     } else {
         myID.className = "bottomMenu hide"
