609dc7cb45999fc81eba879c850b3248a1e4bd66
Tommy <mcgillt95@gmail.com>
2021-12-11 05:13:43 +0000

    LevelSelectFix

--- B00358619/public/Game/scenes/LevelSelectScene.js
+++ B00358619/public/Game/scenes/LevelSelectScene.js
@@ -1,26 +1,24 @@
-import GameScene from "./GameScene.js";
-
-let LevelSelectSceneBool;
 class LevelSelectScene extends Phaser.Scene {
     constructor() {
-        super({key: 'PauseScene'});
+        super({key: 'LevelSelectScene'});
     }
 
     preload(){
         this.load.image('levelSelect_Background','./assets/LevelSelectWithoutButton.png');
+        this.load.image('button1','./assets/button1.png');
+        this.load.image('button2','./assets/button2.png');
         this.load.image('mainMenu_Button','./assets/mainMenuButton.png');
     }
 
     create(){
         // Set background image
         let background = this.add.sprite(400,300,'levelSelect_Background');
-        const button = this.add.sprite( 400, 100, 'mainMenuButton.png')
+        const button1 = this.add.sprite( 316, 239, 'button1')
             .setInteractive()
-            .on('pointerdown', function () {
-                LevelSelectSceneBool = true;
-            });
-        LevelSelectSceneBool = false;
-
+            .on('pointerdown', () => this.scene.start('GameScene'));
+        const button2 = this.add.sprite( 372, 239, 'button2')
+            .setInteractive()
+            .on('pointerdown', () => this.scene.start('GameScene'));
 
     }
 
